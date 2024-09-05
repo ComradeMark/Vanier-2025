641cc4052dc50e3fb7eead0e94e8ee33774a2443
Mark <jm.partners00@gmail.com>
2024-09-05 09:46:45 -0400

    Added September 5th examples

--- Vanier2025Exercises/src/main/java/edu/vanier/Class1/MainApp.java
+++ Vanier2025Exercises/src/main/java/edu/vanier/Class1/MainApp.java
@@ -1,9 +1,14 @@
 package edu.vanier.Class1;
+import edu.vanier.ui.OptionStage;
 import javafx.application.Application;
 import javafx.scene.Group;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+
+import javax.swing.text.html.Option;
+
 /**
  * This is a JavaFX project template to be used for creating GUI applications.
  * JavaFX 20.0.2 is already linked to this project in the build.gradle file.
@@ -18,17 +23,30 @@ public class MainApp extends Application{
         
 //To launch the JFX component of the application, we use LAUNCH
         launch(args);
-        
-        
+    }
+    @Override
+    public void init(){
+        System.out.println("Initialization in progress...");
+    }
+
+    public void stop(){
+        System.out.println("Shutting down application...");
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        System.out.println("Starting application...");
 //At first we must create a scene graph and pass it to the scene
     Group root= new Group();
     //1a. We can now add UI controls to the scene graph
-    Button btnSave = new Button("Save");
+    Button btnSave = new Button("Options");
     root.getChildren().add(btnSave);
+    btnSave.setOnAction((event) -> {
+        System.out.println("Opening new stage");
+        OptionStage stageOptions = new OptionStage(primaryStage, "Options Window");
+        stageOptions.show();
+
+    });
     
 //Creates an FX scene
     Scene scene = new Scene(root, 300, 300);
--- Vanier2025Exercises/src/main/java/edu/vanier/ui/OptionStage.java
+++ Vanier2025Exercises/src/main/java/edu/vanier/ui/OptionStage.java
@@ -1,2 +1,39 @@
-package edu.vanier.ui;public class OptionStage {
+package edu.vanier.ui;
+
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class OptionStage extends Stage {
+
+    public OptionStage(Stage owner, String title) {
+        this.setTitle(title);
+        initOwner(owner);
+        initComponents();
+        //Makes stage a modal window, forcing user interaction before exit is possible
+        initModality(Modality.APPLICATION_MODAL);
+
+    }
+    private void initComponents(){
+        //Create new stage object
+        Stage optionsStage = new Stage();
+
+        //Create a scenegraph for options window
+        HBox optionsRoot = new HBox();
+        Button btnClose = new Button("Close");
+        Button btnSave = new Button("Save");
+        btnClose.setOnAction((event) ->{
+            Platform.exit();
+        });
+
+        optionsRoot.getChildren().addAll(btnClose, btnSave);
+        //Set scenegraph on scene
+        Scene optionsScene = new Scene(optionsRoot, 200, 200);
+        //Set scene on stage
+        setScene(optionsScene);
+    }
+
 }
