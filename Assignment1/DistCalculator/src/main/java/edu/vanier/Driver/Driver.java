23ae2e99cbdb9d582904cb11974d04b737f8f378
Mark Moutter <2354983@edu.vaniercollege.qc.ca>
2024-09-16 11:01:30 -0400

    Added gitignore

--- Assignment1/DistCalculator/src/main/java/edu/vanier/Driver/Driver.java
+++ Assignment1/DistCalculator/src/main/java/edu/vanier/Driver/Driver.java
@@ -6,7 +6,6 @@ public class Driver {
     public static void main(String[] args) {
         System.out.println("Hello from Driver...");
         String csvPath = Driver.class.getResource("/data/postalcodes.csv").getFile();
-        //String csvPath = Driver.class.getResource("/data/postalcodes.csv");
 
     }
 }
