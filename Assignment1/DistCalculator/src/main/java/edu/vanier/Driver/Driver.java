91cfbc98b38aeb0e29b6fd706c7632159a195c95
Mark Moutter <2354983@edu.vaniercollege.qc.ca>
2024-09-16 10:51:12 -0400

    Fixed template

---
+++ Assignment1/DistCalculator/src/main/java/edu/vanier/Driver/Driver.java
@@ -0,0 +1,12 @@
+package edu.vanier.Driver;
+
+
+public class Driver {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Driver...");
+        String csvPath = Driver.class.getResource("/data/postalcodes.csv").getFile();
+        //String csvPath = Driver.class.getResource("/data/postalcodes.csv");
+
+    }
+}
